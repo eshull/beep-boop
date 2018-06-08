f785fc488c0b7825fd9993a8bce4eafcc3d4ff2d
Eric Shull <eshulldesign@gmail.com>
2018-06-08 10:03:42 -0700

    add a function to run throught the users input number

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,7 @@
 
-// 
-// for (var i = 0; i < array.length; i++) {
-//   array[i]
-// }
+
+
+
 
 
 $(document).ready(function() {
@@ -11,7 +10,22 @@ $(document).ready(function() {
     event.preventDefault();
 
     var milesToTravel = parseInt($("#userMiles").val());
-    console.log(milesToTravel);
+
+    var milesArray = function(miles) {
+      for (var i = 0; i < milesToTravel.length; i++) {
+        if (milesToTravel[i] >= 0) {
+          var poppedMiles = milesToTravel.slice();
+          return poppedMiles;
+        }
+      }
+    };
+
+    // var milesArray = function(miles) {
+    //     var poppedMiles = miles.splice();
+    //     return poppedMiles;
+    // };
+    // console.log(poppedMiles);
+    console.log(milesArray(milesToTravel));
 
   });
 
