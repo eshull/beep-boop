08d9bfcf9ac8c1b36c2dc34b3c93eb3ff0f058c5
Eric Shull <eshulldesign@gmail.com>
2018-06-08 13:22:54 -0700

    add function to turn number into string and store in array

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,26 @@ var countedNumbers = function(number) {
 countedNumbers = array;
 }
 
+var numbersByDigits = function(integer) {
+
+  var number = integer
+  var i = number
+      output = [],
+      stringNumber = number.toString();
+
+  for (i = 0; i < stringNumber.length; i ++) {
+    output.slice(stringNumber.charAt(i));
+    output.slice(stringNumber.charAt(i));
+      // output.push(+stringNumber.charAt(i));
+      // output.push(+stringNumber.charAt(i));
+      // console.log(output);
+      console.log(output);
+      return output;
+
+  }
+
+}
+
 
 
 $(document).ready(function() {
@@ -23,9 +43,19 @@ $(document).ready(function() {
 
     for (var i = 0; i < countedNumbers.length; i++) {
       if ((countedNumbers[i] % 3) === 0) {
-        console.log("divisible by 3");
+        console.log("divisible by 3")
+      } else if (i < countedNumbers.length) {
+
+          numbersByDigits(i);
+            console.log(numbersByDigits(i));
+          // "convert number to array to compare each digit"
+          // if (number has 1) {
+          //   "output Boop"
+          // } else if (number has 0) {
+          //   "output beep"
+          // }
       } else {
-        console.log("not working");
+        console.log("display nothing for this number");
       }
     }
 
