4b24296367ebdf83b6562c4bc28a21818d8a6ca7
Eric Shull <eshulldesign@gmail.com>
2018-06-08 09:21:52 -0700

    add jquery to collect info on submit from form

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+
+
+for (var i = 0; i < array.length; i++) {
+  array[i]
+}
+
+
+$(document).ready(function() {
+
+  $("#theForm").submit(function(event) {
+    event.preventDefault();
+
+    var milesToTravel = $
+
+
+  });
+
+
+
+
+});
