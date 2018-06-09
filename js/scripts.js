f29dff74536e7f788b4d122292769b6c40edc7f7
Eric Shull <eshulldesign@gmail.com>
2018-06-08 21:13:56 -0700

    add jquery to toggle div results

--- js/scripts.js
+++ js/scripts.js
@@ -1,146 +1,20 @@
-//
-// var countedNumbers = function(number) {
-//   var i = 0;
-//   var array = [];
-//   while (i < number) {
-//     i++;
-//     // c
 
-// onsole.log(i);
-//     array.push(i)
-//   }
-// countedNumbers = array;
-// }
-//
-// var numbersByDigits = function(integer) {
-//
-//   var number = integer.toString();
-//   // console.log(number);
-//   var stringNumber = number.split("");
-//   var output = [];
-//
-//   for (var i = 0; i < stringNumber.length; i ++) {
-//       // output.pop((i));
-//       // output.pop((i));
-//       output.push(stringNumber.pop());
-//         output.push(stringNumber.pop());
-//         // output.push(stringNumber.pop());
-//         // output.push(stringNumber.pop());
-//         // output.push(stringNumber.pop());
-//         // output.push(stringNumber.pop());
-//         // output.push(stringNumber.pop());
-//         // output.push(stringNumber.pop());
-//         // output.push(stringNumber.pop());
-//         return output;
-//         // numbersByDigits.push(output);
-//       // output.push(+stringNumber.charAt(i));
-//       // output.push(+stringNumber.charAt(i));
-//       // console.log(output);
-//       // console.log(output);
-//       // return output;
-//
-//   }
-// }
-//
-// // var numbersByDigits = function(integer) {
-// //   var quoteArray = integer.split("");
-// // }
-// // function zeroOrOne(a, b) {
-// //   var zero = a
-// //   var one = b
-// //   if (zero === 0) {
-// //     return 'it has a zero';
-// //   } else if (one === 1) {
-// //     return 'it has a one';
-// //   } else {
-// //     return 'Does NOT fit!';
-// //   }
-// //
-// // }
-//
-//
-// $(document).ready(function() {
-//
-//   $("#theForm").submit(function(event) {
-//     event.preventDefault();
-//
-//     var milesToTravel = parseInt($("#userMiles").val());
-//
-//     // countedNumbers(milesToTravel);
-//     alert(countedNumbers);
-//
-//
-//
-//
-//     for (i = 0; i < countedNumbers.length; i++) {
-//       // if (i === 0) { console.log("it continued"); i = (i+1);}
-//       if (((countedNumbers[i] - 1) % 3) === 0) {
-//         var replace = ["I'm sorry, Dave. I'm afraid I can't do that."];
-//         countedNumbers[i] = replace
-//         console.log("divisible by 3" + replace)
-//         // debugger;
-//
-//       } else {
-//         console.log("else statement");
-//         countedNumbers(milesToTravel);
-//           var isItAOne = numbersByDigits(i);
-//           console.log(isItAOne);
-//           // console.log(isItAOne);
-//             for (var i = 0; i < isItAOne.length; i++) {
-//
-//
-//               if (isItAOne[i] === "1") {
-//                 console.log("boop");
-//                 var boop = ["boop"];
-//                 isItAOne[i] = boop
-//                 // return boop;
-//                 isItAOne[i] === countedNumbers.length;
-//                 // break;
-//               } else if (isItAOne[i] === "0") {
-//                 console.log("beep");
-//                 var beep = ["beep"];
-//                 isItAOne[i] = beep
-//                 // return beep;
-//                 isItAOne[i] === countedNumbers.length;
-//                 // break;
-//               } else if (isItAOne[i] <= countedNumbers.length){
-//                 console.log("loser");
-//                 var empty = ("")
-//                 isItAOne[i] = empty;
-//                 isItAOne[i] === countedNumbers.length;
-//               // return (empty);
-//               // debugger;
-//                }
-//                else {
-//                 countedNumbers[i] === countedNumbers.length;
-//                }
-//                // countedNumbers[i] = countedNumbers.length;
-//             }
-// countedNumbers[i] === countedNumbers.length;
-//   console.log(boop + beep + empty);
-//
-//         }
-//   return (boop + beep + empty)
-//         // return (boop + beep + empty)
-//       }
-//   });
-// });
-
+// function to count out all numbers up to input
 var countedNumbers = function(number) {
   var i = 0;
   var array = [];
   while (i < number) {
     i++;
-    // console.log(i);
     array.push(i)
   }
 countedNumbers = array;
 }
 
+// function to turn numbers into a string in an array
 var numbersByDigits = function(integer) {
 
   var number = integer.toString();
-  // console.log(number);
+
   var stringNumber = number.split("");
   var output = [];
 
@@ -156,17 +30,14 @@ $(document).ready(function() {
   $("#theForm").submit(function(event) {
     event.preventDefault();
 
-    var milesToTravel = parseInt($("#userMiles").val());
+    // $(".showResults").show();
+    // $(".results").hide();
 
+
+    var milesToTravel = parseInt($("#userMiles").val());
     countedNumbers(milesToTravel);
     var finalArray = [];
-
-
-
-    for (i = 0; i < countedNumbers.length; i++) {
-
-      console.log(isItAOne);
-
+    for (i = 1; i <= countedNumbers.length; i++) {
 
       if (((countedNumbers[i] - 1) % 3) === 0) {
         var replace = [' "I am sorry, Dave. I am afraid I cannot do that." '];
@@ -174,9 +45,6 @@ $(document).ready(function() {
         console.log("divisible by 3" + replace)
         finalArray.push(replace)
         countedNumbers[i] === countedNumbers.length;
-
-
-
 
       } else {
         console.log("here");
@@ -205,22 +73,21 @@ $(document).ready(function() {
                  // isItAOne[k] = ([k] + 1)
                 isItAOne[i] === countedNumbers.length;
               }
-              //  else {
-              //   var empty = ("")
-              //   finalArray.push(empty);
-              // }
               break;
             }
           }
-
-      // countedNumbers[i] === countedNumbers.length;
       }
       console.log(finalArray);
       var finalQuote = finalArray.join(" ");
       // alert(finalQuote);
-      $(".showResults").html("<div>"+ finalQuote + "</div>");
-      $(".showResults").show();
+      // $(".showResults").addClass();
 
+
+      // $(".showResults").show();
+      // $(".results").show();
+
+
+      // $(".showResults").toggle();
 
   });
 
